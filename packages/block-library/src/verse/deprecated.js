696fe2d6ec7c19b47297f2491c10a227c0a0824f
SirLouen <sir.louen@gmail.com>
2026-01-19 01:39:09 +0100

    Verse Block: Add new textAlign support

--- packages/block-library/src/verse/deprecated.js
+++ packages/block-library/src/verse/deprecated.js
@@ -12,6 +12,7 @@ import { RichText, useBlockProps } from '@wordpress/block-editor';
  * Internal dependencies
  */
 import migrateFontFamily from '../utils/migrate-font-family';
+import migrateTextAlign from '../utils/migrate-text-align';
 
 const v1 = {
 	attributes: {
@@ -85,6 +86,81 @@ const v2 = {
 	},
 };
 
+const v3 = {
+	attributes: {
+		content: {
+			type: 'rich-text',
+			source: 'rich-text',
+			selector: 'pre',
+			__unstablePreserveWhiteSpace: true,
+			role: 'content',
+		},
+		textAlign: {
+			type: 'string',
+		},
+	},
+	supports: {
+		anchor: true,
+		background: {
+			backgroundImage: true,
+			backgroundSize: true,
+		},
+		color: {
+			gradients: true,
+			link: true,
+		},
+		dimensions: {
+			minHeight: true,
+		},
+		typography: {
+			fontSize: true,
+			__experimentalFontFamily: true,
+			lineHeight: true,
+			__experimentalFontStyle: true,
+			__experimentalFontWeight: true,
+			__experimentalLetterSpacing: true,
+			__experimentalTextTransform: true,
+			__experimentalTextDecoration: true,
+			__experimentalWritingMode: true,
+		},
+		spacing: {
+			margin: true,
+			padding: true,
+		},
+		__experimentalBorder: {
+			radius: true,
+			width: true,
+			color: true,
+			style: true,
+		},
+		interactivity: {
+			clientNavigation: true,
+		},
+	},
+	save( { attributes } ) {
+		const { textAlign, content } = attributes;
+
+		const className = clsx( {
+			[ `has-text-align-${ textAlign }` ]: textAlign,
+		} );
+
+		return (
+			<pre { ...useBlockProps.save( { className } ) }>
+				<RichText.Content value={ content } />
+			</pre>
+		);
+	},
+	migrate: migrateTextAlign,
+	isEligible( attributes ) {
+		return (
+			!! attributes.textAlign ||
+			!! attributes.className?.match(
+				/\bhas-text-align-(left|center|right)\b/
+			)
+		);
+	},
+};
+
 /**
  * New deprecations need to be placed first
  * for them to have higher priority.
@@ -93,4 +169,4 @@ const v2 = {
  *
  * See block-deprecation.md
  */
-export default [ v2, v1 ];
+export default [ v3, v2, v1 ];
